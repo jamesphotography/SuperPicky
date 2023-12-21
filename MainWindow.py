efffb45494e9ffc775639834056295a67b0604c6
jamesphotography <james@jamesphotography.com.au>
2023-12-21 18:11:24 +1030

    set up skeleton UI

--- MainWindow.py
+++ MainWindow.py
@@ -1,24 +1,43 @@
-import sys
-from PyQt6.QtWidgets import QMainWindow, QLabel, QVBoxLayout, QWidget
+import os
+import time
+from PyQt6.QtWidgets import QDialog, QFileDialog
+from main_ui import Ui_Dialog  # Import from generated UI file
+from find_bird_util import log_message, run_super_picky
 
-class MainWindow(QMainWindow):
-    def __init__(self):
-        super().__init__()
+class MainWindow(QDialog, Ui_Dialog):
+    def __init__(self, parent=None):
+        super().__init__(parent)
+        self.setupUi(self)
 
-        # Set window title and size
-        self.setWindowTitle("SuperPicky")
-        self.setGeometry(100, 100, 1100, 800)
+        self.directoryPath = ""
 
-        # Create a layout and a label widget
-        layout = QVBoxLayout()
-        label = QLabel("Hello, PyQt6!")
+        # Setup connections
+        self.browse_dir_button.clicked.connect(self.on_browse_button_clicked)
+        self.confirm_button.accepted.connect(self.accept)
+        self.confirm_button.rejected.connect(self.reject)
 
-        # Add the label to the layout
-        layout.addWidget(label)
+    def on_browse_button_clicked(self):
+        # Open a dialog to select a directory
+        directory = QFileDialog.getExistingDirectory(self, "Select Directory")
+        if os.path.exists(directory):
+            self.directoryPath = directory
+            self.display_dir_box.setText(directory)
+            print(f"Selected directory: {self.directoryPath}")
 
-        # Create a central widget and set the layout
-        central_widget = QWidget()
-        central_widget.setLayout(layout)
+    def accept(self):
+        # Code to run when the confirm button is accepted
+        if not os.path.exists(self.directoryPath):
+            return None
 
-        # Set the central widget of the window
-        self.setCentralWidget(central_widget)
+        start = time.time()
+        run = run_super_picky(self.directoryPath)
+        end = time.time()
+
+        log_message(f"Processing time: {end - start}, run = {run}", self.directoryPath)
+
+    def reject(self):
+        self.directoryPath = ""
+        self.display_dir_box.setText("")
+        # Code for cancelling the process
+        print("Process cancelled.")
+        return None
